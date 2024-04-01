e8a3c01254ee81ac08922b5a80c3e95d98e5b5d5
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-04-02 03:44:20 +0600

    entity created for user

--- Cafe-Management-Backend/src/main/java/com/roze/entity/User.java
+++ Cafe-Management-Backend/src/main/java/com/roze/entity/User.java
@@ -1,14 +1,29 @@
 package com.roze.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
 
 @Entity
 @DynamicUpdate
 @DynamicInsert
 @Data
-public class User {
-    
+public class User implements Serializable {
+    private static final long SerialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String contactNumber;
+    private String email;
+    private String password;
+    private String status;
+    private String role;
+
 }
