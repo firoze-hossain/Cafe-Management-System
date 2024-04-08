ec4e424c9f9ca71332a96f82c432f844a6c4e363
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-04-09 04:38:46 +0600

    update category api is done

--- Cafe-Management-Backend/src/main/java/com/roze/service/CategoryService.java
+++ Cafe-Management-Backend/src/main/java/com/roze/service/CategoryService.java
@@ -10,4 +10,6 @@ public interface CategoryService {
     ResponseEntity<String> addNewCategory(Map<String, String> requestMap);
 
      ResponseEntity<List<Category>> getAllCategory(String filterValue);
+
+    ResponseEntity<String> updateCategory(Map<String, String> requestMap);
 }
--- Cafe-Management-Backend/src/main/java/com/roze/service/impl/CategoryServiceImpl.java
+++ Cafe-Management-Backend/src/main/java/com/roze/service/impl/CategoryServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -54,6 +55,29 @@ public class CategoryServiceImpl implements CategoryService {
             ex.printStackTrace();
         }
         return new ResponseEntity<List<Category>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateCategory(Map<String, String> requestMap) {
+        try {
+            if (jwtFilter.isAdmin()) {
+                if (validateCategoryMap(requestMap, true)) {
+                    Optional optional = categoryRepository.findById(Integer.parseInt(requestMap.get("id")));
+                    if (!optional.isEmpty()) {
+                        categoryRepository.save(getCategoryFromMap(requestMap, true));
+                        return CafeUtils.getResponseEntity("Category updated successfully", HttpStatus.OK);
+                    } else {
+                        return CafeUtils.getResponseEntity("Category id does not exist", HttpStatus.NOT_FOUND);
+                    }
+                }
+                return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+            } else {
+                return CafeUtils.getResponseEntity(CafeConstants.UNAUTHORIZED_ACCESS, HttpStatus.UNAUTHORIZED);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private boolean validateCategoryMap(Map<String, String> requestMap, boolean validate) {
