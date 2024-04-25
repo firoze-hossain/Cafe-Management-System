ccfade44a363b1e40797d19fac80bc9da98b12ac
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-04-25 14:22:51 +0600

    forgot password page created

--- Cafe-Management-Frontend/src/app/home/home.component.ts
+++ Cafe-Management-Frontend/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
 import {SignupComponent} from "../signup/signup.component";
+import {ForgotPasswordComponent} from "../forgot-password/forgot-password.component";
 
 @Component({
   selector: 'app-home',
@@ -21,4 +22,9 @@ export class HomeComponent implements OnInit {
     this.dialog.open(SignupComponent, dialogConfig);
   }
 
+  handleForgotPasswordAction() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = "550px";
+    this.dialog.open(ForgotPasswordComponent, dialogConfig);
+  }
 }
--- Cafe-Management-Frontend/src/app/services/user.service.ts
+++ Cafe-Management-Frontend/src/app/services/user.service.ts
@@ -15,4 +15,9 @@ export class UserService {
     return this.httpClient.post(this.url + "/user/signup", data,
       {headers: new HttpHeaders().set('Content-Type', 'application/json')})
   }
+
+  forgotPassword(data: any) {
+    return this.httpClient.post(this.url + "/user/forgotPassword", data,
+      {headers: new HttpHeaders().set('Content-Type', 'application/json')})
+  }
 }
