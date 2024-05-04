5755b5b0200018a91bd15e01f49f5e7eaca26e41
Md. Firoze Hossain <firoze.hossain@outlook.com>
2024-05-04 13:55:09 +0600

    get products page done

--- Cafe-Management-Frontend/src/app/material-component/manage-product/manage-product.component.ts
+++ Cafe-Management-Frontend/src/app/material-component/manage-product/manage-product.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {NgxUiLoaderService} from "ngx-ui-loader";
+import {MatDialog} from "@angular/material/dialog";
+import {SnackbarService} from "../../services/snackbar.service";
+import {Router} from "@angular/router";
+import {MatTableDataSource} from "@angular/material/table";
+import {GlobalConstants} from "../../shared/global-constants";
 
 @Component({
   selector: 'app-manage-product',
@@ -6,10 +13,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-product.component.scss']
 })
 export class ManageProductComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'categoryName', 'description', 'price', 'edit'];
+  dataSource: any;
+  responseMessage: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private productService: ProductService,
+              private ngxService: NgxUiLoaderService,
+              private dialog: MatDialog,
+              private snackbarService: SnackbarService,
+              private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.ngxService.start();
+    this.tableData();
+  }
+
+  tableData() {
+    this.productService.getProducts().subscribe((res: any) => {
+      this.ngxService.stop();
+      this.dataSource = new MatTableDataSource(res);
+    }, (error) => {
+      this.ngxService.stop();
+      console.log(error.error?.message);
+      if (error.error?.message) {
+        this.responseMessage = error.error?.message;
+      } else {
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  handleAddAction() {
+
+  }
+
+  handleEditAction(values:any) {
+
+  }
+
+  handleDeleteAction(values:any) {
+
+  }
+
+  onChange(status:any, id:any) {
+
+  }
 }
